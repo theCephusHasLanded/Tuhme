72ab9ffb23c2708f31ef827d9b286ddf7fc8953e
christinacephus@pursuit.org <christinacephus@pursuit.org>
2025-05-07 12:16:30 -0400

    Configure Firebase for deployment and add user authentication

---
+++ tuhme-vite/src/firebase/config.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDjKOZydtbcJ_ieIr4Aebn5yTSkT_Msv2g",
+  authDomain: "tuhme-22ee9.firebaseapp.com",
+  projectId: "tuhme-22ee9",
+  storageBucket: "tuhme-22ee9.firebasestorage.app",
+  messagingSenderId: "131350866531",
+  appId: "1:131350866531:web:39470ff40dced049276656",
+  measurementId: "G-T012PR80ZC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize services
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+export { app, analytics, db, auth, storage };
